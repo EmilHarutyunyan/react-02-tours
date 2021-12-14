5a47f6d25c36ca24555de4681948270535c92921
EmilHarutyunyan <emil.h098@gmail.com>
2021-12-15 00:20:30 +0400

    tours

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
+function App() {
+  const [loading, setLoading] = useState(true)
+  const [tours, seTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => id !== tour.id)
+    seTours(newTours)
+  }
+
+  const fetchTours = async () => {
+
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      seTours(tours)
+
+    } catch (error) {
+      setLoading(false)
+      console.log(error);
+    }
+
+  }
+  useEffect(() => {
+    fetchTours()
+  },[])
+  
+  if(loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  if(tours.length === 0) {
+    return <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button className="btn" onClick={fetchTours}>refresh</button>
+      </div>
+    </main>
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
+  )
+}
+
+export default App
